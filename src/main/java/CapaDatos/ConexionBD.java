052dbeefe93b51cff49600c85c69e555da725a4c
Lelis Carlos <Lelis Carlos@DESKTOP-1VM3ET1>
2025-09-21 16:48:09 -0500

    Creacion de: ProveedorDAL ProveedorBL FrmProveedor

--- src/main/java/CapaDatos/ConexionBD.java
+++ src/main/java/CapaDatos/ConexionBD.java
@@ -4,10 +4,31 @@
  */
 package CapaDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Lelis Carlos
  */
 public class ConexionBD {
+    Connection cn = null;
     
+    public Connection getConexion(){
+        try{
+            cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/regustrogastos?user=root&password=Lelis955&"
+                    +"allowPublicKeyRetrieval=true&useSSL=false");
+        }catch(Exception el){
+            System.out.println("Error Sql :"+el.getMessage());
+        }
+        return cn;
+    }
+    
+    public void cerrarConexion(){
+        try{
+            cn.close();
+        }catch(Exception el){
+            System.out.println("Error al Cerrar Conexion : "+el.getMessage());
+        }
+    }
 }
---
+++ src/main/java/CapaNegocio/ProveedorBL.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaNegocio;
+
+import CapaDatos.ProveedorDAL;
+import java.util.List;
+
+/**
+ *
+ * @author Lelis Carlos
+ */
+public class ProveedorBL {
+   /* ProveedorDAL oProveedorDAL = new ProveedorDAL();
+    public List listarCompañia(){
+        return oProveedorDAL.listar();
+    }
+    public int agregarProveedor(Proveedor oproveedor){
+        return oProveedorDAL.agregar(oproveedor);
+    }
+    public int actualizarProveedor(Proveedor oproveedor){
+        return oProveedorDAL.actualizar(oproveedor);
+    }
+    public int eliminarProveedor(int id){
+        return oProveedorDAL.eliminar(id);
+    }
+    public Proveedor buscarProveedor(int id){
+        return oProveedorDAL.buscar(id);
+    }*/ 
+}
